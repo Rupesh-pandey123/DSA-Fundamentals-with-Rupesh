df8bc0f113b78f9305e863da8bbce60a94d0bb18
Rupesh Pandey <rupeshpandey201@gmail.com>
2026-04-22 01:40:15 +0530

    Pointetr2arr

--- Dsa/oppsconcept/pointer2arr.cpp
+++ Dsa/oppsconcept/pointer2arr.cpp
@@ -9,5 +9,29 @@ int main()
     cout<<arr+0<<endl;
     cout<<&arr[0]<<endl;
     cout<<ptr<<endl;
+    // print the address of second  element or 1 index
+    cout<<arr+1<<endl;
+    cout<<&arr[1]<<endl;
+    //print value of zero index
+    cout<<arr[0]<<endl;
+    cout<<*arr<<endl;
+    cout<<*(arr+0)<<endl;
+    cout<<*ptr<<endl;
 
+    //all address print 
+    for(int i=0;i<5;i++)
+    cout<<arr+i<<endl;
+
+    //all value  print 
+    for(int i=0;i<5;i++)
+    cout<<*(arr+i)<<endl;
+    //get all address 
+    for(int i=0;i<5;i++)
+    {
+      cout<<ptr+1<<endl;
+
+    }
+    //addition 
+    ptr = ptr+3;
+    cout<<*ptr;
 }
